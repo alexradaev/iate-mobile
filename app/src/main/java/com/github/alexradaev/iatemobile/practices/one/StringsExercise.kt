3e0fdb6c377a6e7571a91fd4e799e547f60f4b56
CAMPUS\AVRadaev <rad.yyhh@yandex.ru>
2021-10-11 22:28:21 +0300

    fixed split a text into sentences

--- app/src/main/java/com/github/alexradaev/iatemobile/practices/one/StringsExercise.kt
+++ app/src/main/java/com/github/alexradaev/iatemobile/practices/one/StringsExercise.kt
@@ -6,7 +6,7 @@ package com.github.alexradaev.iatemobile.practices.one
 class StringsExercise {
     val text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris faucibus porttitor tincidunt. Sed orci libero, efficitur id eleifend sit amet, hendrerit quis nibh. Cras et commodo lectus. Maecenas sem libero, condimentum semper condimentum non, mollis viverra massa. Donec cursus maximus gravida. Pellentesque vitae cursus magna, at egestas nulla. Ut eget augue eget magna mattis convallis a sed nisl. Donec ut porta lorem, et dapibus dolor. Pellentesque ac erat tortor. Donec a est at ante bibendum aliquam eu non enim. Suspendisse ut purus ut lacus tincidunt pretium. Etiam ultrices elit risus, sit amet sagittis felis tempus quis. In at est accumsan, malesuada metus vel, cursus lacus. Etiam ac sodales felis."
 
-    val numOfSentences = text.split(".").filter{ !it.isEmpty() }.map{ it.trim() }.count()
+    val numOfSentences = text.split(".", "?", "!").filter{ !it.isEmpty() }.map{ it.trim() }.count()
     val words = text.split(" ").filter{ it.all { it.isLetter() } }
     val numOfWords = words.count()
     val averageWordLength = words.map{ it.length }.average()
